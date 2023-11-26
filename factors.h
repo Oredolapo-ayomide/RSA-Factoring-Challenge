3f4b276e92d44ff0c572ac6454756216e40ddb2b
Oredolapo-ayomide <117779639+Oredolapo-ayomide@users.noreply.github.com>
2023-11-26 12:18:31 +0100

    rsa challenge

---
+++ factors.h
@@ -0,0 +1,13 @@
+ #ifndef _FACTOR_H_
+  2 #define _FACTOR_H_
+  3
+  4 #include <stdio.h>
+  5 #include <stdlib.h>
+  6 #include <unistd.h>
+  7 #define _GNU_SOURCE
+  8
+  9 /* size_t getline(char **lineptr, size_t *n, FILE *stream); */
+ 10 int factorize(char *buffer);
+ 11
+ 12 /* size_t getline(char **lineptr, size_t *n, FILE *stream); */
+ 13 int factorize(char *buffer);
